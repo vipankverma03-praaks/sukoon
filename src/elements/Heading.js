255e499e00cbf5b2a4c1db2ce0f0076ee6a00c89
adhanadeepak <deepakadhana93@gmail.com>
2019-07-31 17:52:04 +0530

    Title component updated

--- src/elements/Heading.js
+++ src/elements/Heading.js
@@ -1,31 +1,32 @@
 import React from "react";
 import styled from "styled-components";
 
-const Title = (props) =>{
+const Title = (props) => {
 
- const Wrapper = styled.div`
+    const Wrapper = styled.div`
     
- `
-
+    `;
     const SubheadTop = styled.div`
     border-left: 3px solid #ffc300;
     color: #12443d;
     padding-left: 8px;
-`
+    `;
 
     const HeadingWrapper = styled.h2`
-    
-`;
+            
+    `;
+
     const HeadingLight = styled.span`
+      line-height: 1;
      display: ${props => props.display || 'inline-block'};
-`;
+    `;
 
-
-    return(
+    return (
         <Wrapper className={props.wrapperClass}>
             <SubheadTop>{props.subHeading || 'Explore'}</SubheadTop>
-            <HeadingWrapper className="text-sukoon inline text-4xl w-full">
-                <HeadingLight className="font-light mr-2" display={props.display}>{props.titleLight || ''}</HeadingLight>
+            <HeadingWrapper className="text-sukoon text-4xl w-full mt-4 block">
+                <HeadingLight className="font-light mr-2"
+                              display={props.display}>{props.titleLight || ''}</HeadingLight>
                 {props.titleBold}
             </HeadingWrapper>
         </Wrapper>
